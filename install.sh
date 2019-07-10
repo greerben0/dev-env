5faf79188669c7c188aa9e388b9eb99fcab8a11c
greerben0 <greerben0@gmail.com>
2019-07-10 11:22:21 -0600

    Added git lfs install

--- install.sh
+++ install.sh
@@ -19,6 +19,11 @@ brew install wget
 brew install git
 brew link --force git
 
+# install git_lfs
+# https://git-lfs.github.com/
+brew install git-lfs
+git lfs install
+
 # install python3
 brew install python
 
